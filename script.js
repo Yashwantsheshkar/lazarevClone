520d5bee4d4028643af942f0e8c1d8adf1de33d9
Yashwantsheshkar <yashwantsheshkar24@gmail.com>
2024-04-02 01:09:32 +0530

    page

--- script.js
+++ script.js
@@ -39,28 +39,29 @@ ScrollTrigger.refresh();
  
 loco()
 
-var rightElems = document.querySelector(".right-elem")
-rightElems.forEach(function(elem){
-  elem.addEventListener("mouseenter", function(){
-    gsap.to(elem.childNodes[3],{
-      opacity:1,
-      scale:1
-    })
-  })
 
-  elem.addEventListener("mouseleave", function(){
-    
-    gsap.to(elem.childNodes[3],{
-      opacity:0,
-      scale:0
-    })
-  })
 
-  elem.addEventListener("mousemove", function(dets){
-    gsap.to(elem.childNodes[3], {
-      x: dets.x - elem.getBoundingClientRect().x-90, 
-      y: dets.y - elem.getBoundingClientRect().y-190
-    }) 
+
+function page3VideoAnimation(){
+  var page3Center = document.querySelector(".page3-center")
+var video = document.querySelector("#video")
+
+page3Center.addEventListener("click", function(){
+  video.play()
+  gsap.to(video,{
+    transform:"scaleX(1) scaleY(1)",
+    opacity:1,
+    borderRadius: 0,
   })
 })
 
+video.addEventListener("click", function(){
+  video.pause()
+  gsap.to(video,{
+    transform:"scaleX(0.7) scaleY(0)",
+    opacity:0,
+    borderRadius: 0,
+  })
+})
+}
+page3VideoAnimation()
